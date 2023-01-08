ecd4e3a23a66d6fa6c3a48ea6c23fac570bf5a13
sep3r <sep3r@yahoo.com>
2023-01-08 17:03:26 +0330

    feat(add-first Lesson): add first lesson

---
+++ com/sep3r/lesson1/Lesson1Examples.java
@@ -0,0 +1,22 @@
+package com.sep3r.lesson1;
+
+/*
+here i am using first of modifiers like int char double and somthing else
+ */
+
+public class Lesson1Examples {
+
+
+    public static void main(String[] args) {
+        int intex, inex1, inex2;
+        char charex, charex1;
+        double doublex = 1.15;
+        long longex = 1154l;
+        inex2 = 10;
+        charex = 'o';
+        System.out.println(charex);
+        System.out.println(doublex);
+        System.out.println(inex2);
+        System.out.println(longex);
+    }
+}
