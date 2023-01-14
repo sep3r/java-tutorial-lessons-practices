d0fff65abdafb369868ccd3c2cdcef4c36a5cf60
sep3r <sep3r@yahoo.com>
2023-01-14 15:02:03 +0330

    feat(add-lesson0): add lesson 0 && package lesson2

---
+++ com/sep3r/lesson1/Lesson0Examole.java
@@ -0,0 +1,8 @@
+package com.sep3r.lesson1;
+
+public class Lesson0Examole {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello JAVA..!");
+    }
+}
