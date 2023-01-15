417c16dd7a8452cca62d598e1c3740e8ccebea88
sep3r <sep3r@yahoo.com>
2023-01-15 15:23:38 +0330

    feat(add new Lesson): add new lesson

---
+++ com/sep3r/lesson3/Lesson3Example.java
@@ -0,0 +1,4 @@
+package com.sep3r.lesson3;
+
+public interface Lesson3Example {
+}
---
+++ com/sep3r/lesson3/Lesson3ExampleImpl.java
@@ -0,0 +1,5 @@
+package com.sep3r.lesson3;
+
+//Example to implement an Interface
+public class Lesson3ExampleImpl implements Lesson3Example{
+}
