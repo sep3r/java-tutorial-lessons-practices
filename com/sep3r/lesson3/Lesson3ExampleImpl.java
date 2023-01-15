172fb6469dfb92bcd77e9cb7d912b9f3eada9cfe
sep3r <sep3r@yahoo.com>
2023-01-15 17:00:01 +0330

    feat(add-new-inner): add new inner class and interface immplement for practice

--- com/sep3r/lesson3/Lesson3ExampleImpl.java
+++ com/sep3r/lesson3/Lesson3ExampleImpl.java
@@ -3,31 +3,36 @@ package com.sep3r.lesson3;
 import java.util.Scanner;
 
 //Example to implement an Interface
-public class Lesson3ExampleImpl implements Lesson3Example{
+public class Lesson3ExampleImpl{
     public static void main(String[] args) {
         try{
+            Inner inner = new Inner();
+            inner.getMessage();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
-        }catch(Exception e){
-            e.printStackTrace();
+    static class Inner implements Lesson3Example {
+        @Override
+        public void getMessage() {
+            try (Scanner sc = new Scanner(System.in);) {
+                System.out.println("Enter Message to Show ...!");
+                String s = sc.nextLine();
+                writeMessage(s);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-    }
-    @Override
-    public void getMessage() {
-        try (Scanner sc = new Scanner(System.in);){
-            System.out.println("Enter Message to Show ...!");
-            String s = sc.nextLine();
-            writeMessage(s);
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-    }
-    public void writeMessage(String s) {
-        try{
-            System.out.println(s);
-        } catch(NullPointerException e){
-            e.printStackTrace();
-        } catch(Exception e){
-            e.printStackTrace();
+
+        public void writeMessage(String s) {
+            try {
+                System.out.println(s);
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
