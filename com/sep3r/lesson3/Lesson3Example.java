b46891b0d7fce51f21be823fa84a21fe3584ce3e
sep3r <sep3r@yahoo.com>
2023-01-15 16:24:01 +0330

    feat(add-interface): add interface and implement

--- com/sep3r/lesson3/Lesson3Example.java
+++ com/sep3r/lesson3/Lesson3Example.java
@@ -1,4 +1,6 @@
 package com.sep3r.lesson3;
 
 public interface Lesson3Example {
+    void getMessage();
+    void writeMessage(String s);
 }
--- com/sep3r/lesson3/Lesson3ExampleImpl.java
+++ com/sep3r/lesson3/Lesson3ExampleImpl.java
@@ -1,6 +1,33 @@
 package com.sep3r.lesson3;
 
+import java.util.Scanner;
+
 //Example to implement an Interface
 public class Lesson3ExampleImpl implements Lesson3Example{
+    public static void main(String[] args) {
+        try{
 
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void getMessage() {
+        try (Scanner sc = new Scanner(System.in);){
+            System.out.println("Enter Message to Show ...!");
+            String s = sc.nextLine();
+            writeMessage(s);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void writeMessage(String s) {
+        try{
+            System.out.println(s);
+        } catch(NullPointerException e){
+            e.printStackTrace();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
