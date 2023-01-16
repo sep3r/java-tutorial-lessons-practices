a47776d989d5abe1c8eda2e6ac41cfc296a2bbcd
sep3r <sep3r@yahoo.com>
2023-01-16 21:30:40 +0330

    feat(add-list-set): implement list and set into lesson4

--- com/sep3r/lesson4/Lesson4Example.java
+++ com/sep3r/lesson4/Lesson4Example.java
@@ -1,4 +1,26 @@
 package com.sep3r.lesson4;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Lesson4Example {
+
+    //this lesson for Collections
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add("ali");
+            set.add("ali");
+        }
+        for (String s : set) {
+            System.out.println("set :" + s);
+        }
+        for (String s : list) {
+            System.out.println("list :" + s);
+        }
+
+    }
 }
