499e8248f82835a1e6494820a2ccd146da04c67b
sep3r <sep3r@yahoo.com>
2023-01-18 18:27:15 +0330

    feat(add-wrapper): add wrapper class into class

---
+++ com/sep3r/lesson4/WrapperClass.java
@@ -0,0 +1,16 @@
+package com.sep3r.lesson4;
+
+public class WrapperClass <T> {
+    private T value;
+    public WrapperClass() {
+    }
+    public WrapperClass(T value) {
+        this.value = value;
+    }
+    public T getValue() {
+        return value;
+    }
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
