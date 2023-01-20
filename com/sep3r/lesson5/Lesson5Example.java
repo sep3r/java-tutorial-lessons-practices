bb3f16c63205bd0a7b1089360b01d7f1ef7b3749
sep3r <sep3r@yahoo.com>
2023-01-20 21:19:01 +0330

    feat(add lambda): add new lambda with overloading support

--- com/sep3r/lesson5/Lesson5Example.java
+++ com/sep3r/lesson5/Lesson5Example.java
@@ -4,14 +4,20 @@ public class Lesson5Example {
     public static void main(String[] args) {
         Lesson5Example lesson5Example = new Lesson5Example();
         lesson5Example.test((number1, number2)->number1+ number2, 15, 180);
+        lesson5Example.test((str1, str2)->str1+str2, "salam ", " Chetori?");
     }
-    private void test(Checkpoint checkpoint, Integer number1, Integer number2){
-        System.out.println(checkpoint.sum(number1, number2));
+    private void test(Checkpoint checkpoint, Integer x, Integer y){
+        System.out.println(checkpoint.sum(x, y));
+    }
+    private void test(Concatable concatable, String x, String y){
+        System.out.println(concatable.concat(x,y));
     }
     @FunctionalInterface
     interface Checkpoint{
         int sum(Integer x, Integer y);
     }
-
-
+    @FunctionalInterface
+    interface Concatable{
+        String concat(String x, String y);
+    }
 }
